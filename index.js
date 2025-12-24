906ee822d2d6b045b164266d9f891a7be097a45a
fatsermint <kyosti.ahti@gmail.com>
2025-12-24 12:04:34 +0200

    Added speed and light signal data

--- index.js
+++ index.js
@@ -6,18 +6,28 @@ const mqtt = require("mqtt")
 
 const app = express()
 app.use(cors())
-const port = 3000
+const port = 3002
+
+let writeRequested = false
 
 let data = {
     stats: {
         doorOpen: 0,
-        doorClose: 0
+        doorClose: 0,
+    },
+    maxSpeed: {
+        route: null,
+        speed: -1
+    },
+    lightRequest: {
+        request: 0,
+        response: 0
     }
 }
 
 fs.readFile("data.json", (err, newdata) => {
     if (newdata) {
-        data = JSON.parse(newdata)
+        data = { ...data, ...JSON.parse(newdata) }
     }
     client.on("message", (topic, message) => {
         const data = JSON.parse(message.toString())
@@ -31,11 +41,8 @@ app.get('/', (req, res) => {
         api: "HSL Data Api"
     })
 })
-app.get("/dooropen", (req, res) => {
-    res.json({
-        door_open: data.stats.doorOpen
-    }
-    )
+app.get("/stats", (req, res) => {
+    res.json(data)
 })
 
 app.listen(port, () => {
@@ -49,7 +56,7 @@ const client = mqtt.connect("wss://mqtt.hsl.fi/");
 
 
 client.on("connect", () => {
-    client.subscribe("/hfp/v2/journey/ongoing/doo/#", (err) => {
+    client.subscribe("/hfp/v2/journey/ongoing/+/#", (err) => {
         if (err) return console.error("FAILED TO CONNECT", err)
 
     });
@@ -57,7 +64,18 @@ client.on("connect", () => {
 
 
 function handleMessage(topic, type, message) {
-    console.log("message: ", type)
+
+    const [_, prefix, version, journey_type, temporal_type, event_type, transport_mode, operator_id, vehicle_number, route_id, direction_id, headsign, start_time, next_stop, geohash_level, geohash, sid] = topic.split("/")
+    if (message.spd > data.maxSpeed.speed) {
+        const modeBlacklist = ["train", "metro"]
+        if (!modeBlacklist.some(mode => mode == transport_mode)) {
+            data.maxSpeed.route = message.route
+            data.maxSpeed.speed = message.spd
+            console.log(message.spd, message.route)
+        }
+
+    }
+
     switch (type) {
         case "DOO":
             data.stats.doorOpen++
@@ -66,5 +84,26 @@ function handleMessage(topic, type, message) {
         default:
             break;
     }
-    fs.writeFile("data.json", JSON.stringify(data), (err) => { })
+    switch (type) {
+        case "TLR":
+            data.lightRequest.request++
+            break;
+
+        default:
+            break;
+    }
+    switch (type) {
+        case "TLA":
+            data.lightRequest.response++
+            break;
+
+        default:
+            break;
+    }
+    if (!writeRequested) writeRequested = true
 }
+setInterval(() => {
+    if (!writeRequested) return
+    console.log("writing data")
+    fs.writeFile("data.json", JSON.stringify(data), (err) => { })
+}, 5_000)
